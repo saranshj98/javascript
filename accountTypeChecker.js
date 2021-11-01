f10c299d334979db306fa0a4d9f3f55aeabd2953
Saransh <saransh.jain@myglamm.com>
2021-11-01 21:06:39 +0530

    add assumption and code comment

--- accountTypeChecker.js
+++ accountTypeChecker.js
@@ -1,19 +1,42 @@
+/** 
+ * example of input argument
+  const accountBalanceHistory = [
+    {
+      monthNumber: 0, // current month
+      account: {
+        balance: { amount: 0 },
+      },
+    },
+    {
+      monthNumber: 1, // last month
+      account: {
+        balance: { amount: 100 },
+      },
+    },
+    {
+      monthNumber: 2, // two months ago
+      account: {
+        balance: { amount: 200 },
+      },
+    }
+  ]
+ */
+
+/**
+ * have made few assumptions for below function
+ * 1. input argument should be of type array.
+ * 2. the given array should contain atleast 2 or 3 months of data. It should not contain any missing month data.
+ * 3. array is already sorted on month number.
+ */
 const accountTypeChecker = (accountBalanceHistory) => {
   /***
-  Your goal is to write a function that determines from someone's ${accountBalanceHistory} 🧾 (see the array above for an example)
+  Your goal is to write a function that determines from someone's ${accountBalanceHistory}
   whether this array is of type A (variable) or type B (fixed).
 
   Type 🅰 denotes a balance history where the balance amount decreases by varying amounts each month.
 
   Type 🅱 is one where the balance amount changes by the same amount each month.
   ***/
-
-  /**
-   * have made few assumptions for below function
-   * 1. the given array should contain atleast 2 or 3 months of data. It should not contain any missing month data.
-   * 2. array is already sorted on month number.
-   */
-
   let result = "B";
   if (accountBalanceHistory.length === 2) {
     return result;
@@ -32,3 +55,16 @@ const accountTypeChecker = (accountBalanceHistory) => {
     return result;
   }
 };
+
+/**
+ * Note:
+ * 1. yes it solves the basic case.
+ * 2. We can modify the given data structure for optimization. Also we should consider year-wise data.
+ * 3. unit tests:
+ *  a. validate the function with different typeof argument.
+ *  b. validate the result on empty array.
+ *  c. validate the result with multiple records.
+ *  d. validate the result with balance amount decreases by varying amounts each month.
+ *  e. validate the result with balance amount changes by the same amount each month.
+ *  f. check if argument passed contains field "amount" with proper data type.
+ */
